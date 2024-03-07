f488d2166885be8b109ef0b83f46209a8a7960d9
Carmelico92 <130740279+Carmelico92@users.noreply.github.com>
2024-03-07 11:52:35 +0000

    creacion de la funcionalidad del todos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [tarea, setTareas] = useState([]);
+  const [text, setText] = useState('');
+  const [count, setCount] = useState(0);
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' && text.trim() !== '') {  //Añade una linea cada vez que pulsas Enter
+      setText('');
+      setTareas(prevTodos => [
+        {
+          id: prevTodos.length,
+          text: text
+        },
+        ...prevTodos
+      ]);
+      setCount(prevCount => prevCount + 1); // suma en 1 el contenedor por cada linea que se va añadiendo
+    }
+  };
+
+  const handleDelete = (id) => { 
+    setTareas(prevTodos => prevTodos.filter(todo => todo.id !== id)); //Elimina la tarea al pulsar el boton 
+    setCount(prevCount => prevCount - 1); // resta en 1 el contenedor por cada linea que se elimina
+  };
+
+  return (
+    <div className="container flex-column vstack gap-3">
+      <h1 className="text-center">todos</h1>
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onKeyDown={handleKeyDown}
+        placeholder="Escribe tu tarea aquí..."
+      />
+      
+      <ul>
+        {tarea.map(item => (
+          <div className="border list-group-item d-flex justify-content-between align-items-center container-btnDelete" key={item.id}>
+            {item.text}
+            <button onClick={() => handleDelete(item.id)} className="btnDelete">X</button>
+          </div>
+        ))}
+      </ul>
+      <div>
+        <h2>{count} item left</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
+
+
+
